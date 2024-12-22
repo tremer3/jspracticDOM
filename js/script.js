e30b24fb5a1152121e611f05cdc6d84e894aa4ff
Rustam <drbtrmr@gmail.com>
2024-12-22 16:28:40 +0500

    change comedy title

--- js/script.js
+++ js/script.js
@@ -24,8 +24,12 @@ const movieDB = {
     ]
 };
 
-const adv = document.querySelectorAll('.promo__adv img');
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre');
 
 adv.forEach(item => {
     item.remove();
 });
+
+genre.textContent = 'драма';
