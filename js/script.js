5db6e905fd439391ddc5ed1adb903c5462f5b934
Rustam <drbtrmr@gmail.com>
2024-12-23 12:07:36 +0500

    add basket task

--- js/script.js
+++ js/script.js
@@ -99,6 +99,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 </li>
             `;
         })
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+            });
+        });
     }
 
     deleteAdv(adv);
